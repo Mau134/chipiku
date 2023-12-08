5ddd588900b85e041fb45878eb801c8502787af1
Maurice Molande <molande.mau@gmail.com>
2023-12-08 02:46:36 +0200

    dashboard and log in 3

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/style.css">
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <meta name="appleid-signin-client-id" content="your-apple-client-id">
+  <script src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"></script>
   <title>Chipiku Stores login</title>
 </head>
 <body>
@@ -20,6 +23,14 @@
 
         <button type="button" onclick="login()">Login</button>
       </form>
+      <div class="external-signin">
+      <img src="./images/google.jpeg" alt="Google" class="external-signin-icon">
+      <img src="./images/Apple.jpeg" alt="Apple" class="external-signin-icon">
+      </div>
+      <div>
+        <button>Sign in with Google</button>
+        <button>Sign in with Apple</button>
+      </div>
       <p>Don't have an account? <a href="register.php">Register</a></p>
     </div>
   </div>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,52 @@
-function login() {
-    // Add your login logic here
-    alert("Login button clicked!");
-  }
+// Add this in your script.js
+
+// Google Sign-In Logic
+function googleSignIn() {
+  // Use the Google Sign-In API to authenticate the user
+  gapi.auth2.getAuthInstance().signIn().then(onGoogleSignInSuccess).catch(onGoogleSignInFailure);
+}
+
+// Callback function for successful Google Sign-In
+function onGoogleSignInSuccess(googleUser) {
+  // Retrieve user details
+  var profile = googleUser.getBasicProfile();
   
+  // You can now use 'profile' to get user information
+  console.log('Google User ID: ' + profile.getId());
+  console.log('Google User Name: ' + profile.getName());
+  console.log('Google User Email: ' + profile.getEmail());
+
+  // Add additional logic or send data to your server as needed
+}
+
+// Callback function for failed Google Sign-In
+function onGoogleSignInFailure(error) {
+  console.error('Google Sign-In Failed:', error);
+}
+
+// Apple Sign-In Logic
+function appleSignIn() {
+  // Use the Apple Sign-In API to authenticate the user
+  var appleIDAuthRequest = new AppleIDAuthRequest();
+  appleIDAuthRequest.requestOptions = {
+    requestedScopes: ["email", "name"],
+  };
+
+  appleIDAuthRequest.performRequest().then(onAppleSignInSuccess).catch(onAppleSignInFailure);
+}
+
+// Callback function for successful Apple Sign-In
+function onAppleSignInSuccess(response) {
+  // Retrieve user details
+  var user = response.user;
+
+  // You can now use 'user' to get user information
+  console.log('Apple User ID: ' + user);
+
+  // Add additional logic or send data to your server as needed
+}
+
+// Callback function for failed Apple Sign-In
+function onAppleSignInFailure(error) {
+  console.error('Apple Sign-In Failed:', error);
+}
